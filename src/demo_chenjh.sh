9c6c9e6c743f5fd78e9d9dafff2e4eed7703b98d
陈佳辉 <chenjiahui@chenjiahuideMacBook-Air.local>
2019-04-28 02:10:18 +0800

    Shell Demo修复-c选项，不能正确显示带空格的字符串的问题

--- src/demo_chenjh.sh
+++ src/demo_chenjh.sh
@@ -8,11 +8,11 @@ ARG_HELP='
 #   功能：实现循环打印指定文本内容，并可保存log。
 #       该脚本为演示Shell功能，尽量采用多种语法格式
 #   参数：
+#       -h  显示该帮助文本
 #       -l  log保存路径
 #       -e  是否显式显示log
 #       -t  循环次数
 #       -c  文本内容
-#       -h  显示该帮助文本
 #
 ###########################################################################
 '
@@ -22,16 +22,8 @@ ARG_ECHO=true
 ARG_TIMES=5
 ARG_CONTENT="Hello World!"
 
-this_print() {
-	TAG="#"
-	#显示信息
-	$ARG_ECHO && echo $TAG $@
-	#保存LOG
-	[ -f "$ARG_LOG" ] && echo $TAG $@ >> $ARG_LOG
-	return 0
-}
-
-this_arg_func() {
+# 解析选项参数值，并映射功能函数
+this_map_arg_func() {
 	ARG=$1
 	FUNC=$2
 	ARG_VALUE=""
@@ -51,10 +43,40 @@ this_arg_func() {
 	return 1
 }
 
+# 解析选项多个参数值，或字符串，并映射功能函数
+this_map_str_func() {
+	ARG=$1
+	FUNC=$2
+	ARG_VALUE=""
+	shift 2
+	#解析参数值
+	while [ ! "$1" = "" ]; do
+		[ ! "$1" = "$ARG" ] && shift && continue
+		#参数值不为空和“-”开头，则保存参数值
+		while [ ! "$2" = "" ] && ! (echo "$2" | grep -n "^-" >/dev/null); do
+			ARG_VALUE="$ARG_VALUE $2"
+			shift	
+		done
+		#映射函数与参数值
+		$FUNC $ARG_VALUE
+		return 0
+	done
+	return 1
+}
+
+this_print() {
+	TAG="#"
+	#显示信息
+	$ARG_ECHO && echo $TAG $@
+	#保存LOG
+	[ -f "$ARG_LOG" ] && echo $TAG $@ >> $ARG_LOG
+	return 0
+}
+
 func_log() {
 	[ ! -f "$1" ] && touch $1
 	[ ! -f "$1" ] && this_print "-e 参数错误: $1" && return 1
-	
+
 	ARG_LOG=$1
 	this_print "保存路径: $ARG_LOG"
 	return 0
@@ -77,18 +99,18 @@ func_times() {
 }
 
 func_content() {
-	ARG_CONTENT=$1
+	ARG_CONTENT=$*
 }
 
 func_help() {
-    echo "$ARG_HELP"
-    exit 0
+	echo "$ARG_HELP"
+	exit 0
 }
 
 main() {
-    #创建log文件
-    echo " Shell Demo " >> $ARG_LOG
-    this_print "$ARG_LOG, $ARG_ECHO, $ARG_TIMES, $ARG_CONTENT"
+	#创建log文件
+	echo " Shell Demo " >> $ARG_LOG
+	this_print "$ARG_LOG, $ARG_ECHO, $ARG_TIMES, \"$ARG_CONTENT\""
 	this_print "$(date) 开始测试===>"
 
 	while (( i < $ARG_TIMES )); do
@@ -101,13 +123,12 @@ main() {
 	return 0
 }
 
+this_map_arg_func -h func_help $@
 
-this_arg_func -l func_log $@
-this_arg_func -e func_echo $@
-this_arg_func -t func_times $@
-this_arg_func -c func_content $@
-
-this_arg_func -h func_help $@
+this_map_arg_func -l func_log $@
+this_map_arg_func -e func_echo $@
+this_map_arg_func -t func_times $@
+this_map_str_func -c func_content $@
 
 main
 
